0414901c446bb2f52900bb530c02caba847d756c
Ed Khalid <ekhalid@knowland.com>
2022-10-03 01:04:53 -0400

    add router and error page
    a

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from 'react-router-dom'
 import './index.css';
 import { App } from './App';
 import reportWebVitals from './reportWebVitals';
 import { ApolloProvider } from '@apollo/client';
 import { client } from './setupApollo';
+import { ErrorPage } from './components/Misc/Error';
+import { AdminPage } from './components/Admin/AdminPage';
+import { PlayerManager } from './components/Admin/PlayerManager';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App/> ,
+    errorElement: <ErrorPage/>
+  },
+  {
+    path: '/admin',
+    element: <AdminPage/>,
+    errorElement: <ErrorPage/>
+    ,children: [
+      {
+        path: "players",
+        element: <PlayerManager/>
+      }
+    ]
+  }
+])
 
 ReactDOM.render(
   <React.StrictMode>
     <ApolloProvider client={client}>
-      <App />
+      <RouterProvider router ={router}/>
     </ApolloProvider>
   </React.StrictMode>,
   document.getElementById('root')
